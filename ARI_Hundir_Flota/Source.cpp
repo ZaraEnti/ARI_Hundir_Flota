a70390d1fce295a920d507036f470a2fd5aaf4c1
ZaraEnti <151615461+ZaraEnti@users.noreply.github.com>
2024-06-02 12:14:53 +0200

    función impresion definida

--- ARI_Hundir_Flota/Source.cpp
+++ ARI_Hundir_Flota/Source.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 #include<string>
 
+//Inicialización 2 Tablas
+const short tamaño = 10;
+char tablero1[tamaño][tamaño];
+char tablero2[tamaño][tamaño];
 
 //Función actualizar matriz
 void insertar(char valor, std::string nombreTabla, const short tamaño) {
@@ -17,28 +21,33 @@ void insertar(char valor, std::string nombreTabla, const short tamaño) {
 		}
 	}
 }
-
 //Función imprimir
-void imprimir(std::string nombreTablero, const short tamaño) {
+void imprimir(const short tamaño) {
+	std::cout << "Tablero jugador 1" << std::endl;
 	for (short i = 0; i < tamaño; i++) {
+		for (short j = 0; j < tamaño; j++) {
+			std::cout << " " << tablero1[i][j] << std::endl;
 
+		}
+	}
+	std::cout << "Tablero jugador 2" << std::endl;
+	for (short i = 0; i < tamaño; i++) {
+		for (short j = 0; j < tamaño; j++) {
+			std::cout << " " << tablero2[i][j] << std::endl;
+
+		}
 	}
 }
 
 
 void main() {
-
-	//Inicialización 2 Tablas
-	const short tamaño = 10;
-	char tablero1[tamaño][tamaño];
-	char tablero2[tamaño][tamaño];
-
 	// Valores de la tablas por defecto
-		for (short i = 0; i < tamaño; i++) {
-			for (short j = 0; j < tamaño; j++) {
-				tablero1[i][j] = '~';
-				tablero2[i][j] = '~';
-			}
+	for (short i = 0; i < tamaño; i++) {
+		for (short j = 0; j < tamaño; j++) {
+			tablero1[i][j] = '~';
+			tablero2[i][j] = '~';
 		}
+	}
 
+	imprimir(tamaño);
 }
